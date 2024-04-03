adc06a3b31e48543b7caae5b993317ee72140544
Raul Velbaum <raul@kalamaja.com>
2024-04-03 12:49:52 +0300

    Added configuration and dependency injection to test project

---
+++ Tests/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests;
+
+public abstract class TestBase
+{
+    protected readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
+
+    protected readonly ServiceProvider _services = new ServiceCollection()
+        .BuildServiceProvider();
+
+}
