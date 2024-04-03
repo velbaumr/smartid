03dfb76a656b4ab0a9e83df56c3a53ee130fa91b
Raul Velbaum <raul@kalamaja.com>
2024-04-03 13:14:08 +0300

    added empty hashcalculator

---
+++ Services/HashCalculator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Services;
+
+public class HashCalculator(IConfiguration configuration)
+{
+    public static byte[] CalculateHash(byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Tests/TestBase.cs
+++ Tests/TestBase.cs
@@ -5,12 +5,13 @@ namespace Tests;
 
 public abstract class TestBase
 {
-    protected readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+    protected static readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
         .SetBasePath(Path.Combine(AppContext.BaseDirectory))
         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
         .Build();
 
     protected readonly ServiceProvider _services = new ServiceCollection()
+        .AddSingleton<IConfiguration>(_configuration)
         .BuildServiceProvider();
 
 }
