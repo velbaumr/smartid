45f9f825a272b83b96eff0fd473a818e2f26ddf9
Raul Velbaum <raul@kalamaja.com>
2024-04-06 02:29:39 +0300

    ui modified

--- SmartId/Application.cs
+++ SmartId/Application.cs
@@ -14,17 +14,17 @@ public class Application(
             var request = requestBuilder.Build();
             var verificationCode = requestBuilder.VerificationCode;
             Console.WriteLine($"Verification code: {verificationCode}");
-            Console.WriteLine("Enter Id Code or Test Document number");
+            Console.WriteLine("Enter Estonian Id Code or Test Document number");
             var input = Console.ReadLine() ?? string.Empty;
             var result = await authenticator.Authenticate(request,
                 input);
 
             Console.WriteLine(result.Value ?? $"Error: {result.ErrorMessage}");
-            if (result.Value == "Ok")
+            if (result.Value == "OK")
             {
                 Console.WriteLine("You are successfully authenticated.");
             }
-            Console.WriteLine("Do you want to continue (y/any other key)?");
+            Console.WriteLine("Do you want to continue (y to continue/any other key to quit)?");
             var condition = Console.ReadKey()
                 .KeyChar;
 
