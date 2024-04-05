fe4bf7f0a2133db68cd6ace669ba7517e2c80dc0
Raul Velbaum <raul@kalamaja.com>
2024-04-06 02:20:33 +0300

    ui mods

--- SmartId/Application.cs
+++ SmartId/Application.cs
@@ -2,7 +2,9 @@
 
 namespace SmartId;
 
-public class Application(IAuthenticator authenticator, IRequestBuilder requestBuilder)
+public class Application(
+    IAuthenticator authenticator,
+    IRequestBuilder requestBuilder)
 {
     public async Task Run()
     {
@@ -14,18 +16,24 @@ public class Application(IAuthenticator authenticator, IRequestBuilder requestBu
             Console.WriteLine($"Verification code: {verificationCode}");
             Console.WriteLine("Enter Id Code or Test Document number");
             var input = Console.ReadLine() ?? string.Empty;
-            var result = await authenticator.Authenticate(request, input);
+            var result = await authenticator.Authenticate(request,
+                input);
 
-            Console.WriteLine(result.Value ?? result.ErrorMessage);
+            Console.WriteLine(result.Value ?? $"Error: {result.ErrorMessage}");
+            if (result.Value == "Ok")
+            {
+                Console.WriteLine("You are successfully authenticated.");
+            }
             Console.WriteLine("Do you want to continue (y/any other key)?");
-            var condition = Console.ReadKey().KeyChar;
+            var condition = Console.ReadKey()
+                .KeyChar;
 
             if (char.ToUpper(condition) == 'Y')
             {
                 finished = true;
             }
         } while (finished);
-        
+
         Environment.Exit(0);
     }
 }
