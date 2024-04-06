c97cf724c34ca80c1d14194b7925db6f2a4af2c7
Raul Velbaum <raul@kalamaja.com>
2024-04-06 03:57:07 +0300

    ui modifications

--- SmartId/Application.cs
+++ SmartId/Application.cs
@@ -9,31 +9,45 @@ public class Application(
     public async Task Run()
     {
         var finished = false;
-        do
+        
+        while (!finished)
         {
             var request = requestBuilder.Build();
             var verificationCode = requestBuilder.VerificationCode;
-            Console.WriteLine($"Verification code: {verificationCode}");
+            
+            Console.WriteLine($"Verification Code: {verificationCode}");
             Console.WriteLine("Enter Estonian Id Code or Test Document number");
+            Console.WriteLine();
             var input = Console.ReadLine() ?? string.Empty;
             var result = await authenticator.Authenticate(request,
-                input);
-
+                PrepareCode(input));
+            Console.WriteLine();
             Console.WriteLine(result.Value ?? $"Error: {result.ErrorMessage}");
-            if (result.Value == "OK")
+            Console.WriteLine();
+            if (result is { ErrorMessage: not null, HasErrors: true } && result.ErrorMessage.Contains("404"))
             {
-                Console.WriteLine("You are successfully authenticated.");
+                Console.WriteLine("Smart-Id Account Doesn't Exists For Entered Value.");
             }
-            Console.WriteLine("Do you want to continue (y to continue/any other key to quit)?");
+            Console.WriteLine();
+            Console.WriteLine(result.Value == "OK" ? "You Are Successfully Authenticated." : "Authentication Failed.");
+            Console.WriteLine();
+            Console.WriteLine("Do You Want to Continue? (y to Continue/Any Other Key to Quit)");
             var condition = Console.ReadKey()
                 .KeyChar;
 
-            if (char.ToUpper(condition) == 'Y')
+            if (char.ToUpper(condition) != 'Y')
             {
                 finished = true;
             }
-        } while (finished);
+            
+            Console.WriteLine();
+        }
 
         Environment.Exit(0);
+    }
+
+    private static string PrepareCode(string code)
+    {
+        return code.ToUpper().EndsWith("-MOCK-Q") ? code : $"PNOEE-{code}";
     }
 }
